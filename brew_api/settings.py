4dc4f83f933cf329bf2c5bcb1f274a0e56f34c76
matheusbaltar <baltarmatheus20@gmail.com>
2026-06-01 10:03:43 -0300

    migration for docker and postgres

--- brew_api/settings.py
+++ brew_api/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,8 +52,12 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'brewrecipe'),
+        'USER': os.environ.get('POSTGRES_USER', 'brew'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'brew'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
