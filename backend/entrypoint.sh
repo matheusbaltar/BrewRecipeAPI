989a84279e4abb1e978045869236f8806508c6e7
matheusbaltar <baltarmatheus20@gmail.com>
2026-06-11 14:16:23 -0300

    functional entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+# O comando acima avisa o sistema que isso é um script shell (sh).
+# Usamos sh e não bash porque a imagem python:3.12-slim garante o sh,
+# mas não necessariamente o bash. O Dockerfile também chama com "sh entrypoint.sh".
 
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "Starting server..."
+echo "Iniciando o servidor Python..."
 exec "$@"
